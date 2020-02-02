1937fbd994843073ae18b2df277c242150c08db4
Juan Carlos Ruiz <juancarlos.ruiz@pccomponentes.com>
2020-02-02 15:38:41 +0100

    Add theme config

--- src/components/atoms/input/__tests__/input.test.js
+++ src/components/atoms/input/__tests__/input.test.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import {render} from '@testing-library/react'
-import Input from '.'
+import Input from '..'
 
 describe('Input', () => {
-  it('renders the component', () => {
+  test('it works default', () => {
     const {container} = render(<Input />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with id and name attr', () => {
+    const {container} = render(<Input id="id" name="name" />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with placeholder', () => {
+    const {container} = render(<Input placeholder="Placeholder" />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with default value', () => {
+    const {container} = render(
+      <Input placeholder="Placeholder" value="Default value" />
+    )
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with disabled attr', () => {
+    const {container} = render(
+      <Input placeholder="Placeholder" value="Default value" disabled />
+    )
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with readonly attr', () => {
+    const {container} = render(
+      <Input placeholder="Placeholder" value="Default value" readonly />
+    )
     expect(container.firstChild).toMatchSnapshot()
   })
 })
--- src/components/atoms/input/index.js
+++ src/components/atoms/input/index.js
@@ -2,12 +2,42 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './index.scss'
 
-const Input = ({...props}) => {
+const Input = ({
+  id,
+  name,
+  placeholder,
+  value,
+  disabled,
+  readonly,
+  ...props
+}) => {
   return (
-    <div className="FormField">
-      <input className="FormField-input" {...props} />
-    </div>
+    <input
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      defaultValue={value}
+      disabled={disabled}
+      readOnly={readonly}
+      className="Input"
+      {...props}
+    />
   )
+}
+
+Input.propTypes = {
+  /* The DOM id global attribute */
+  id: PropTypes.string,
+  /* Sets the name property of an element in the DOM */
+  name: PropTypes.string,
+  /* A hint to the user of what can be entered in the control. The placeholder text must not contain carriage returns or line-feeds */
+  placeholder: PropTypes.string,
+  /* Default value of the input */
+  value: PropTypes.string,
+  /* This Boolean attribute prevents the user from interacting with the input */
+  disabled: PropTypes.bool,
+  /* This Boolean attribute prevents the user modify the value of input */
+  readonly: PropTypes.bool
 }
 
 export default Input
