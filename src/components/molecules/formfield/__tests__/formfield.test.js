6098d5b890f3f9eea7fe4d96dafe220d72b59c04
Juan Carlos Ruiz <juancarlos.ruiz@pccomponentes.com>
2020-02-02 20:42:40 +0100

    Update component

--- src/components/molecules/formfield/__tests__/formfield.test.js
+++ src/components/molecules/formfield/__tests__/formfield.test.js
@@ -3,8 +3,55 @@ import {render} from '@testing-library/react'
 import FormField from '..'
 
 describe('FormField', () => {
-  test('renders the component', () => {
-    const {container} = render(<FormField label="Default label" name="input" />)
+  test('it works default', () => {
+    const {container} = render(
+      <FormField label="Default label" name="FormField" />
+    )
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with placeholder', () => {
+    const {container} = render(
+      <FormField label="Default label" name="input" placeholder="Placeholder" />
+    )
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with default value', () => {
+    const {container} = render(
+      <FormField
+        label="Default label"
+        name="input"
+        placeholder="Placeholder"
+        value="Default value"
+      />
+    )
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with disabled attr', () => {
+    const {container} = render(
+      <FormField
+        label="Default label"
+        name="input"
+        placeholder="Placeholder"
+        value="Default value"
+        disabled
+      />
+    )
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with readonly attr', () => {
+    const {container} = render(
+      <FormField
+        label="Default label"
+        name="input"
+        placeholder="Placeholder"
+        value="Default value"
+        readonly
+      />
+    )
     expect(container.firstChild).toMatchSnapshot()
   })
 })
--- src/components/molecules/formfield/index.js
+++ src/components/molecules/formfield/index.js
@@ -4,7 +4,15 @@ import Label from '../../atoms/label'
 import Input from '../../atoms/input'
 import './index.scss'
 
-const FormField = ({label, name, onClick, readonly, ...props}) => {
+const FormField = ({
+  label,
+  name,
+  value,
+  onClick,
+  disabled,
+  readonly,
+  ...props
+}) => {
   return (
     <div className="FormField">
       {label && (
@@ -15,6 +23,8 @@ const FormField = ({label, name, onClick, readonly, ...props}) => {
       <Input
         type="text"
         id={name}
+        defaultValue={value}
+        disabled={disabled}
         readOnly={readonly}
         onClick={onClick}
         {...props}
@@ -24,10 +34,20 @@ const FormField = ({label, name, onClick, readonly, ...props}) => {
 }
 
 FormField.propTypes = {
+  /** The label itself */
   label: PropTypes.string.isRequired,
+  /** Sets the name property of an element in the DOM */
   name: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
-  readonly: PropTypes.bool
+  /** A hint to the user of what can be entered in the control. The placeholder text must not contain carriage returns or line-feeds */
+  placeholder: PropTypes.string,
+  /** Default value of input */
+  value: PropTypes.string,
+  /** This Boolean attribute prevents the user from interacting with the input */
+  disabled: PropTypes.bool,
+  /** This Boolean attribute prevents the user modify the value of input */
+  readonly: PropTypes.bool,
+  /** Click event */
+  onClick: PropTypes.func
 }
 
 export default FormField
