57704307fef8e53023e57f3a2327693784c5c3c3
Juan Carlos Ruiz <juancarlos.ruiz@pccomponentes.com>
2020-02-02 15:41:31 +0100

    Import components to entry point

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 // Atoms
-import Button from './components/atoms/Button'
-import Input from './components/atoms/Input'
-import Label from './components/atoms/Label'
-import Popover from './components/atoms/Popover'
+import Button from './components/atoms/button'
+import Heading from './components/atoms/heading'
+import Icon from './components/atoms/icon'
+import Input from './components/atoms/input'
+import Label from './components/atoms/label'
+import Popover from './components/atoms/popover'
 
 // Molecules
-import Calendar from './components/molecules/Calendar'
-import Datepicker from './components/molecules/Datepicker'
-import FormField from './components/molecules/FormField'
+import Calendar from './components/molecules/calendar'
+import Datepicker from './components/molecules/datepicker'
+import FormField from './components/molecules/formfield'
 
 export default {
   Button,
+  Heading,
+  Icon,
   Input,
   Label,
   Popover,
