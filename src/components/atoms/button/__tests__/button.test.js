3e9bb0e207e574e1c0bbe29cf1fef8739aeeba6d
Juan Carlos Ruiz <juancarlos.ruiz@pccomponentes.com>
2020-02-02 20:41:27 +0100

    Add variations

--- src/components/atoms/button/__tests__/button.test.js
+++ src/components/atoms/button/__tests__/button.test.js
@@ -3,8 +3,20 @@ import {render} from '@testing-library/react'
 import Button from '..'
 
 describe('Button', () => {
-  test('renders the component', () => {
+  test('it works default', () => {
     const {container} = render(<Button>Default button</Button>)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works disabled', () => {
+    const {container} = render(<Button disabled>Default button</Button>)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('it works with onClick event', () => {
+    const {container} = render(
+      <Button onClick={() => console.log('onClick')}>Default button</Button>
+    )
     expect(container.firstChild).toMatchSnapshot()
   })
 })
--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -2,15 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './index.scss'
 
-const Button = ({onClick, children, ...props}) => (
-  <button className="Button" onClick={onClick} {...props}>
+const Button = ({onClick, disabled, children, ...props}) => (
+  <button className="Button" disabled={disabled} onClick={onClick} {...props}>
     {children}
   </button>
 )
 
 Button.propTypes = {
-  onClick: PropTypes.func,
-  children: PropTypes.node.isRequired
+  /** Content to be included in the button */
+  children: PropTypes.node.isRequired,
+  /** This Boolean attribute prevents the user from interacting with the button */
+  disabled: PropTypes.bool,
+  /** Click event */
+  onClick: PropTypes.func
 }
 
 export default Button
